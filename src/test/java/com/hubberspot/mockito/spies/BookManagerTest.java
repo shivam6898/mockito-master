94d3481e5696c41e3acb5754dbb058e50a72c9f1
Shivam Patil <shivampatil727@gmail.com>
2022-07-25 17:09:22 +0530

    Comment Added.

--- src/test/java/com/hubberspot/mockito/spies/BookManagerTest.java
+++ src/test/java/com/hubberspot/mockito/spies/BookManagerTest.java
@@ -27,7 +27,9 @@ public class BookManagerTest {
 		// We need to mock findBook because it is communicating to database or not implemented
 		// We need to call getAppliedDiscount to calculate the discounted price
 		Book book = new Book("1234", "Mockito In Action", 500, LocalDate.now());
+		//with spied Object  use doReturn.when syntax
 		doReturn(book).when(bookService).findBook("1234");
+		//  it  will  actually  call  the findBook method
 //		when(bookService.findBook("1234")).thenReturn(book);
 		int actualDiscount = bookManager.applyDiscountOnBook("1234", 10);
 		assertEquals(450, actualDiscount);
